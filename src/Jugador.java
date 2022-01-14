51611c8645a8a3d79d5fc8578f77792fd04f6417
ReyesMCuesta <91255447+ReyesMCuesta@users.noreply.github.com>
2022-01-14 11:04:07 +0100

    Initial commit

---
+++ src/Jugador.java
@@ -0,0 +1,37 @@
+
+public class Jugador {
+	private String nombre;
+	private int edad;
+	private String idioma;
+	
+	public Jugador (String nombre, int edad, String idioma) {
+		this.setNombreJugador(nombre);
+		this.setEdad(edad);
+		this.setIdioma(idioma);
+		
+		
+		}
+	
+	public void setNombreJugador(String nombre) {
+		//code here
+	}
+	
+	public void setEdad(int edad) {
+		//code here
+	}
+	public void setIdioma(String idioma) {
+		//code here
+	}
+	
+
+	public String getIdioma() {
+		return idioma;
+	}
+	
+	public String getNombreJugador() {
+		return nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+}
